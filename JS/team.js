9d0fd82085afa5e0a9d370bd4e4816bf38dc4958
Johannes Zarre <zarre.johannes@gmail.com>
2020-09-25 20:46:08 +0200

    finished team.js

--- JS/team.js
+++ JS/team.js
@@ -1,13 +1,16 @@
 console.log(employees)
 console.log(employees.length)
+console.log(employees[0].name)
 
 let nose =document.getElementById("container");
 
 
+
 for (let i = 0; i< employees.length; i++){
 	let head = document.createElement("div")
-	head.innerText = employees[i];
 	nose.appendChild(head);
-	nose.setAttribute("class","teamcard")
-	
-}
+	head.setAttribute("class",employees[i].name);
+	head.innerHTML = ` <img class="teamimg" src='${employees[i].image}'><br>`+ employees[i].name + "<br>" + employees[i].email;
+
+	}
+
