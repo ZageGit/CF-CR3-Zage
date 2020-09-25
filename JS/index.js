5efc8f0d23fa744e63c3fc742f35422d46a56f9b
Johannes Zarre <zarre.johannes@gmail.com>
2020-09-25 17:10:25 +0200

    started footer

--- JS/index.js
+++ JS/index.js
@@ -7,16 +7,16 @@ function calculateInsurance () {
 
 	var d = parseInt(document.getElementById("HP").value);
 
-	var cost
+	var cost 
 
 if (c=="Austria"){
-	cost = d*100/b+50;
+	cost = Math.round(d*100/b+50);
 		
 	} else if(c=="Hungary"){
-	cost= d*120/b+100;
+	cost= Math.round(d*120/b+100);
 		
 	} else {
-	cost = d*150/(b+3)+50;
+	cost = Math.round(d*150/(b+3)+50);
 		
 	}
 
@@ -34,24 +34,3 @@ return (cost)
 
 document.getElementById("myButton").addEventListener("click",calculateInsurance,false);
 
-
-/*	
-switch (d) {
-		case 'Austria':
-			var cost = d*100/b+50;
-			break
-		case 'Hungary':
-			var cost = d*120/b+100;
-			break
-		default:
-			var cost = d*150 / (b+3) +50;
-	}
-	console.log(cost)
-
-if (c="Austria"){
-		document.getElementById("result").innerHTML = parseInt(d) * 100 / parseInt(b) +50;
-	} else if(c="Hungary"){
-		document.getElementById("result").innerHTML = parseInt(d) * 120 / parseInt(b) +100;
-	} else {
-		document.getElementById("result").innerHTML = parseInt(d) * 150 / (parseInt(b)+3) +50;
-	}*/
