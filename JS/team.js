e73bb20222cc3b91ac20e54f3a40facf1add7240
Johannes Zarre <zarre.johannes@gmail.com>
2020-09-25 23:31:46 +0200

    working team.js

--- JS/team.js
+++ JS/team.js
@@ -1,16 +1,25 @@
-console.log(employees)
-console.log(employees.length)
-console.log(employees[0].name)
-
 let nose =document.getElementById("container");
-
 
 
 for (let i = 0; i< employees.length; i++){
 	let head = document.createElement("div")
 	nose.appendChild(head);
-	head.setAttribute("class","teamcard");
-	head.innerHTML = ` <img class="teamimg" src='${employees[i].image}'><br>`+ employees[i].name + "<br>" + employees[i].email;
-
+	head.setAttribute("class","teamcard"+ i);
+	head.addEventListener('click',expand,false)
+	head.innerHTML = ` <img class="teamimg" src='${employees[i].image}'`+ "<br>" + '<p>' + employees[i].name +'</p>'+ '<p>' + employees[i].email +  '</p>' + "<div id=moreInfo"+i+"</div>";
 	}
+
+	function expand() {
+	document.getElementById("moreInfo").innerHTML = `<p>Name: ${employees.name} </p>  
+      <p>E-Mail: ${employees.email}</p>`;
+
+}
+
+
+/*	el.style.backgroundColor = 'grey';
+}
+var el =document.getElementsByTagName("teamcard")
+var elementNode = document.querySelectorAll(".teamcard");
+*/
+
 
