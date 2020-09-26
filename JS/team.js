7cdbd9227849971d2be026b5522ad54b7fcf8f79
Johannes Zarre <zarre.johannes@gmail.com>
2020-09-26 15:41:37 +0200

    changed team.js logic

--- JS/team.js
+++ JS/team.js
@@ -1,19 +1,19 @@
+var empl = JSON.parse(employees);
+console.log(empl)
+
 let nose =document.getElementById("container");
 
-
-for (let i = 0; i< employees.length; i++){
+for (let i = 0; i< empl.length; i++){
 	let head = document.createElement("div")
 	nose.appendChild(head);
 	head.setAttribute("class","teamcard"+ i);
 	head.addEventListener('click',expand,false)
-	head.innerHTML = ` <img class="teamimg${[i]}" src='${employees[i].image}'`+ '<br>' + '<p>' + employees[i].name +'</p>'+ '<p>' + employees[i].email +  '</p>' + "<div id=moreInfo"+i+">"+"</div>";
+	head.innerHTML = ` <img class="teamimg${[i]}" src='${empl[i].image}'`+ '<br>' + '<p>' + empl[i].name +'</p>'+ '<p>' + empl[i].email +  '</p>' + "<div id=moreInfo"+i+">"+"</div>";
 	}
 
-	function expand(i) {
-	document.getElementById("moreInfo" + i).innerHTML = `<p>name: ${employees[i].name} </p><p>E.Mail: ${employees[i].email}`;
-      
-
-}
+	function expand(x) {
+	document.getElementById("moreInfo" + x).innerHTML = `<p>name: ${empl[x].name} </p><p>E.Mail: ${empl[x].email}`;
+	}
 
 
 
