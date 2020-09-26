cffdb79b69fa02c4e5b945bbc60d1f03135cbabb
Johannes Zarre <zarre.johannes@gmail.com>
2020-09-26 16:47:23 +0200

    finished CR

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,9 @@
+
+
+// Function for calculating Insurance
+
 function calculateInsurance () {
+	// defining html container for grabbing values
 	var a = document.getElementById("name").value;
 	
 	var b = parseInt(document.getElementById("age").value);
@@ -6,32 +11,22 @@ function calculateInsurance () {
 	var c = document.getElementById("country").value;
 
 	var d = parseInt(document.getElementById("HP").value);
-
-	var cost 
-
+// if statement for different calculating methods based on Country
 if (c=="Austria"){
 	cost = Math.round(d*100/b+50);
-		
 	} else if(c=="Hungary"){
-	cost= Math.round(d*120/b+100);
-		
-	} else {
+	cost= Math.round(d*120/b+100);		
+	} else{
 	cost = Math.round(d*150/(b+3)+50);
-		
+	} 
+	//if statement to give out message if needed values ar not integers
+	if (isNaN(cost)){
+		document.getElementById('result').innerHTML= "Please enter valid Values.";
+	 }
+	else {document.getElementById('result').innerHTML= a + " your insurance costs are " + cost + " €";
 	}
-	
-
-	
-console.log((typeof a)+ " " + a);
-console.log((typeof b)+ " " + b);
-console.log((typeof c)+ " " + c);
-console.log((typeof d)+ " " + d);
-console.log((typeof cost)+ " " + cost);
-document.getElementById('result').innerHTML= a + " your insurance costs are " + cost + " €";
-return (cost)
-	
 
 }
-
+//adding eventlistener to button for executing Function calculateInsurance on click
 document.getElementById("myButton").addEventListener("click",calculateInsurance,false);
 
